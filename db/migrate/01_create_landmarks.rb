e612c226a00fe26c8272a517061fd2c7d8a31a7f
Ben Blackman <bblkmn5@gmail.com>
2017-12-10 17:04:50 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration[5.1]
-  create_table :landmarks do |t|
-    t.text :name
-    t.integer :figure_id
-    t.integer :year_completed
+  def change
+    create_table :landmarks do |t|
+      t.text :name
+      t.integer :figure_id
+      t.integer :year_completed
+    end
   end
 end
