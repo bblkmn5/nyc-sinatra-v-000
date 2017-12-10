ec4abf54af6e76f26d0b5e85c9857611025d8144
Ben Blackman <bblkmn5@gmail.com>
2017-12-10 18:28:14 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -18,7 +18,7 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
-    erb :"/figures/#{@figure.id}/edit"
+    erb :"/figures/#{@figures.id}/edit"
   end
 
   post '/figures' do
