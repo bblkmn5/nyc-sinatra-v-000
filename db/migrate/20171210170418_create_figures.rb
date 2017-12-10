6c4fd12b0454d141e16e34fda6e7f509e3f48119
Ben Blackman <bblkmn5@gmail.com>
2017-12-10 17:05:14 +0000

    automatically backed up by learn

--- db/migrate/20171210170418_create_figures.rb
+++ db/migrate/20171210170418_create_figures.rb
@@ -1,4 +1,7 @@
-class CreateFigures < ActiveRecord::Migration
+class CreateFigures < ActiveRecord::Migration[5.1]
   def change
+    create_table :figures do |t|
+      t.text :name
+    end
   end
 end
