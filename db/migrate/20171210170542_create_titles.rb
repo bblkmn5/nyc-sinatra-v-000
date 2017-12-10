20e43d9a63da611e8d61b1adf9b94427a534b9d9
Ben Blackman <bblkmn5@gmail.com>
2017-12-10 17:06:04 +0000

    automatically backed up by learn

--- db/migrate/20171210170542_create_titles.rb
+++ db/migrate/20171210170542_create_titles.rb
@@ -1,4 +1,7 @@
-class CreateTitles < ActiveRecord::Migration
+class CreateTitles < ActiveRecord::Migration[5.1]
   def change
+    create_table :titles do |t|
+      t.text :name
+    end
   end
 end
